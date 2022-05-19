5e37600d5eac2cdc0c53c17d6901bf5c5eec46b2
vemundss <vemund@live.com>
2022-05-19 13:18:39 +0200

    moves more util-methods here

--- ca2-mec/utils.py
+++ ca2-mec/utils.py
@@ -1,11 +1,38 @@
 import numpy as np
+import numpy.ma as ma
+
+
+def nancorrcoef(X, Y):
+    """
+    Correlate X, Y (2D arrays) while ignoring nans
+    """
+    X = ma.masked_array(X, mask=np.isnan(X))
+    Y = ma.masked_array(Y, mask=np.isnan(Y))
+    return ma.corrcoef(X.flatten(), Y.flatten())[0, 1]
+
+
+def action_is_recording(action_id):
+    return ("type" in project.require_action(action_id).attributes) and (
+        project.require_action(action_id).attributes["type"] == "Recording"
+    )
+
+
+def get_entity_actions(entity):
+    return [action_id for action_id in actions if action_id[:3] == entity]
+
+
+def trial_id(action):
+    for tag in action.attributes["tags"]:
+        if "trial_id" in tag:
+            return tag.split("_")[-1]
 
 
 def trial_label(action):
-    for tag in action.attributes['tags']:
-        if 'trial_id' in tag:
-            trial_num = int(tag.split('_')[-1][1])
-            return trial_num
+    """
+    Returns the trial label (number) for the action.
+    """
+    return int(trial_id(action)[1])
+
 
 def social_label(action):
     """
@@ -13,50 +40,56 @@ def social_label(action):
     there are four types of socializing: nobox (-1) empty (0), familiar (1) or novel (2).
     this gives a 4d-vector with four possible categories each.
     """
-    
+
     def social_category(str1, str2):
-        """ helper function """
-        if str1 == 'nobox':
+        """helper function"""
+        if str1 == "nobox":
             return -1
-        if str2 == 'e':
+        if str2 == "e":
             return 0
-        if str2 == 'f':
+        if str2 == "f":
             return 1
-        if str2 == 'n':
+        if str2 == "n":
             return 2
-    
-    tags = action.attributes['tags']
-    social_types = {'s': np.zeros(4), 'o': np.zeros(4)}
+
+    tags = action.attributes["tags"]
+    social_types = {"s": np.zeros(4), "o": np.zeros(4)}
     for tag in tags:
-        if not 'corner' in tag:
+        if not "corner" in tag:
             continue
-        stag = tag.split('_')[1:]
+        stag = tag.split("_")[1:]
         # TR, TL, BL and BR, respectively
-        if stag[1] == 'tr':
+        if stag[1] == "tr":
             social_types[stag[0]][0] = social_category(*stag[-2:])
-        elif stag[1] == 'tl':
+        elif stag[1] == "tl":
             social_types[stag[0]][1] = social_category(*stag[-2:])
-        elif stag[1] == 'bl':
+        elif stag[1] == "bl":
             social_types[stag[0]][2] = social_category(*stag[-2:])
-        elif stag[1] == 'br':
+        elif stag[1] == "br":
             social_types[stag[0]][3] = social_category(*stag[-2:])
-    
+
     return social_types
+
+
+def rotmat(theta):
+    """
+    Returns a 2D rotation matrix for theta degrees.
+    """
+    theta = np.radians(theta)
+    r = np.array(((np.cos(theta), -np.sin(theta)), (np.sin(theta), np.cos(theta))))
+    return r
 
 
 def transform_coordinates(x, y, theta=90, **kwargs):
     """
     Transform tracking coordinates to align with physical coordinates
-    For this project (CA2 MEC): rotate recorded coordinates 90 degrees, 
+    For this project (CA2 MEC): rotate recorded coordinates 90 degrees,
     followed by a shift to make values positive afterwards.
     """
     # rotate x,y coordinates 90 degrees using a 2D rotation matrix transform
-    theta = np.radians(theta)
-    r = np.array(( (np.cos(theta), -np.sin(theta)),
-                   (np.sin(theta),  np.cos(theta)) ))
-    coords = r @ np.array([x,y])
+    coords = rotmat(theta) @ np.array([x, y])
     # shift new x-coordinates to be positive
-    coords -= np.array([[-1],[0]])
+    coords -= np.array([[-1], [0]])
     return coords
 
 
@@ -64,29 +97,31 @@ def corner_masks(x, y, margin=0.4, **kwargs):
     """
     Find corner and center of box masks.
     """
-    assert margin < 0.5 and margin > 0, "OBS! Margin must be positive and max half the box size, i.e. 0.5 OBS!"
-    
+    assert (
+        margin < 0.5 and margin > 0
+    ), "OBS! Margin must be positive and max half the box size, i.e. 0.5 OBS!"
+
     # center x and y coordinates
     x = x - 0.5
     y = y - 0.5
     pos = np.array([x, y]).T
-    
+
     # create cardinal basis vectors
     ex, ey = np.arange(2), np.arange(2)[::-1]
-    
+
     # create corner vectors: TR, TL, BL and BR, respectively
-    corners = np.array([ex+ey, -ex+ey, -ex-ey, ex-ey])
-    
-    corner_masks = np.zeros((len(x), 5),dtype=bool)
+    corners = np.array([ex + ey, -ex + ey, -ex - ey, ex - ey])
+
+    corner_masks = np.zeros((len(x), 5), dtype=bool)
     for i, corner in enumerate(corners):
         # find which quadrant positions are in, and if they are within the
         # box margins. The intersection gives the grand mask.
         quadrant_mask = ((pos * corner) > 0).all(axis=-1)
         margin_mask = (np.abs(pos) > (np.ones(2) * (0.5 - margin))).all(axis=-1)
-        corner_masks[:,i] = quadrant_mask & margin_mask
-    
+        corner_masks[:, i] = quadrant_mask & margin_mask
+
     # inverse of union over corner masks
-    corner_masks[:,-1] = ~np.array(corner_masks).any(axis=-1)
+    corner_masks[:, -1] = ~np.array(corner_masks).any(axis=-1)
     return corner_masks
 
 
@@ -97,10 +132,33 @@ def truncate_recording(arr, t, t_start=0.0, t_stop=None):
     """
     if not isinstance(arr, np.ndarray):
         return arr
-    mask = np.ones(len(arr),dtype=bool)
+    mask = np.ones(len(arr), dtype=bool)
     mask = (t > t_start) if t_start is not None else mask
     mask = mask & (t < t_stop) if t_stop is not None else mask
     return arr[mask]
+
+def truncate_tracking_dict(tracking_dict, t_start=0.0, t_stop=None):
+    """
+    Truncate tracking dict, in seconds.
+    if t_start or t_stop is None, then don't truncate
+    """
+    t = np.copy(tracking_dict["t"])
+    for key in tracking_dict:
+        tracking_dict[key] = truncate_recording(tracking_dict[key], t, t_start, t_stop)
+    return tracking_dict
+
+
+def truncate_spikes(spike_times, t):
+    """
+    Truncate spikes to the same length as the recording.
+    """
+    scope_realtime = t[[0, -1]]
+    # scope spike times
+    spike_times_mask = (spike_times > scope_realtime[0]) & (
+        spike_times < scope_realtime[1]
+    )
+    spike_times = spike_times[spike_times_mask]
+    return spike_times
 
 def persistent_units(spikes):
     """
