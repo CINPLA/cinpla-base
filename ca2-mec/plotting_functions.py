05c817be5381f2520f1181c14ad702dc40e078b4
Vemund Schøyen <vemund@live.com>
2022-06-30 13:26:31 +0200

    adds spikes and tracking plot

--- ca2-mec/plotting_functions.py
+++ ca2-mec/plotting_functions.py
@@ -1,3 +1,20 @@
+from scipy.interpolate import interp1d
+import numpy as np
+
+
+def spike_track(x,y,t,spike_train,ax,spines=True):
+    ax.plot(x,y,color='grey',alpha=0.5,zorder=0)
+    spike_pos_f = interp1d(t, np.stack([x,y],axis=0), kind='linear', fill_value='extrapolate')
+    spike_pos = spike_pos_f(spike_train.times)
+    ax.scatter(*spike_pos,color=(0.7, 0.2, 0.2), zorder=1)
+    axis_off_labels_on(ax)
+    # re-add spines
+    if spines:
+        for spine in ax.spines.values():
+            spine.set_visible(True)
+    # set x and y limits (boundary)
+    ax.axis([0,1,0,1])
+    return ax
 
 
 def set_size(width=345.0, fraction=1, mode='wide'):
